05a558bb890deb2b6d6f196e88f7c1d3fddc884a
Sathananthan Januskar <139611014+SathanJa@users.noreply.github.com>
2024-12-16 19:56:27 +0000

    edit

--- 13.12.Practical/index.php
+++ 13.12.Practical/index.php
@@ -1,9 +1,11 @@
-
 <html lang="HTML">
 <head>
 
     <title>Parking Management App</title>
     <style>
+        <
+        style type = "text/css"
+        >
         .main {
             margin: 40px;
             padding: 20px;
@@ -84,8 +86,9 @@
 </head>
 <body>
 <?php
-require_once 'conf.php';
-require_once 'func.php';
+require_once 'conf/conf.php';
+require_once 'func/func.php';
+$e1=false; $e2=false; $e3=false; $e4=false;
 ?>
 <div class="main">
     <div class="head">
@@ -104,25 +107,20 @@ require_once 'func.php';
         <div class="left">
             <div class="parking-container">
                 <?php
-
-                $q = "SELECT parking_slot, vehicle_no FROM parkinglog";
-
-                $data = GetTableData($q,$connection);
-
-                foreach ($data as $key => $value) {
-                    if($value['vehicle_no']=='EMPTY'){
+                $query = "select * from parkinglog";
+                $slots = GetTableData($query, $connection);
+                foreach ($slots as $slot) {
+                    $id = $slot['parking_slot'];
+                    $vno = $slot['vehicle_no'];
+                    if ($vno == 'EMPTY') {
                         echo "<div>";
+                    } else {
+                        echo "<div style='background-color: coral'>";
                     }
-                    else{
-                        echo "<div style='background-color:#FF5555'>";
-                    }
-                    echo $value['parking_slot'];
-                    echo "<br>";                    
-                    echo $value['vehicle_no'];
-                    echo "</div>";
+                    echo "$id<br>$vno</div>";
                 }
-
                 ?>
+
             </div>
         </div>
         <div class="right">
@@ -131,12 +129,22 @@ require_once 'func.php';
                     <tr>
                         <td align="right">Option:</td>
                         <td>
-                        <label>
-                            <input type="radio" name="park" value="alloc" checked onclick="toggleInput(this)"> Allocate
-                            <input type="radio" name="park" value="free" onclick="toggleInput(this)"> Free
-                        </label>
+                            <label>
+                                <input type="radio" name="park" value="alloc">Allocate
+                                <input type="radio" name="park" value="free">Free
+
+                            </label>
                         </td>
                         <td>
+
+                                <?php
+                                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                                    echo "<label class='error'>";
+                                    $e4 = RequiredField("park", "Please select the option");
+                                    echo "</label>";
+                                }
+                                ?>
+
 
                         </td>
                     </tr>
@@ -144,39 +152,47 @@ require_once 'func.php';
                         <td>Slot No:</td>
                         <td align="right">
                             <label>
-                                <input type="number" name="slot" placeholder="Enter Slot No">
+                                <input type="number" name="slot">
                             </label>
                         </td>
                         <td>
                             <label class="error">
-                            <?php
-
-                            if($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST['slot'])){
-                                RequiredField($_POST['slot'],'please enter slot number <br>');
-                            }
-
-                            ?>
+                                <?php
+                                if ($_SERVER['REQUEST_METHOD'] == 'POST' && $e4) {
+                                    echo "<label class='error'>";
+                                    $e1 = RequiredField("slot", "Please provide the slot number");
+                                    echo "</label>";
+                                    if ($_POST['park'] == 'alloc') {
+                                        if ($e1) {
+                                            echo "<label class='error'>";
+                                            $e3 = ValidateSlot("slot", $connection);
+                                            echo "</label>";
+                                        }
+                                    }
+                                }
+                                ?>
                             </label>
 
                         </td>
                     </tr>
-                    <tr id="vnoLabel" style="display: ;">
+                    <tr>
                         <td>Vehicle No:</td>
                         <td align="right">
                             <label>
-                                <input type="text" name="vno" placeholder="Enter Vehicle No">
+                                <input type="text" name="vno">
                             </label>
                         </td>
                         <td>
-                            <label class="error" id="error">
-                            <?php
-
-                            if($_SERVER['REQUEST_METHOD']=='POST' && $_POST['park']=='alloc' && empty($_POST['vno'])){
-                                RequiredField($_POST['vno'],'please enter vehicle number <br>');
-                            }
-
-                            ?>
-                                
+                            <label class="error">
+                                <?php
+                                if ($_SERVER['REQUEST_METHOD'] == 'POST' && $e4) {
+                                    if ($_POST['park'] == 'alloc') {
+                                        echo "<label class='error'>";
+                                        $e2 = RequiredField("vno", "Please provide the vehicle number");
+                                        echo "</label>";
+                                    }
+                                }
+                                ?>
                             </label>
                         </td>
                     </tr>
@@ -194,6 +210,19 @@ require_once 'func.php';
                     </tr>
                     <tr>
                         <td colspan="2">
+                            <?php
+                            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                                if($e4){
+                                    if ($e1 && $e3) {
+                                        $slot = $_POST['slot'];
+                                        $vno = $_POST['vno'];
+                                        $park = $_POST['park'];
+                                        ParkingUpdate($slot, $vno, $park, $connection);
+                                    }
+                                }
+
+                            }
+                            ?>
                         </td>
                     </tr>
                 </table>
@@ -202,50 +231,8 @@ require_once 'func.php';
     </div>
 </div>
 
-<?php
-
-if($_SERVER['REQUEST_METHOD']=='POST' && $_POST['slot']!=''){
-    $parkType = $_POST['park'];
-    $slot = $_POST['slot'];
-    $vehiNo = $_POST['vno'];
-
-    if($_POST['slot']!='' && $_POST['park']!=''){
-        if($parkType=='alloc'){
-            $q = "SELECT parking_slot FROM parkinglog WHERE id=$slot AND vehicle_no='EMPTY'";
-            $result = mysqli_query($connection,$q);
-            if (mysqli_num_rows($result)==1){
-                $q = "UPDATE parkinglog SET vehicle_no='$vehiNo' WHERE id=$slot";
-                mysqli_query($connection,$q);
-                echo "<script> location.replace('index.php'); </script>";
-                //header('location:index.php');
-            }
-            else{
-                $q = "SELECT vehicle_no FROM parkinglog WHERE id=$slot";
-                $result = mysqli_query($connection,$q);
-                $row = mysqli_fetch_row($result);
-                echo "<script>document.getElementById('error').innerHTML = 'This slot is not availbale, allready allocate to ".$row[0]."';</script>";
-            }
-        }
-        else{
-            $q = "UPDATE parkinglog SET vehicle_no='EMPTY' WHERE id=$slot";
-            $result = mysqli_query($connection,$q);
-            echo "<script> location.replace('index.php'); </script>";
-            //header('location:index.php');
-        }
-    }
-
-}
-?>
-
-<script>
-        function toggleInput(radio) {
-            const vnoLabel = document.getElementById('vnoLabel');
-            if (radio.value === "free") {
-                vnoLabel.style.display = "none"; // Hide input
-            } else {
-                vnoLabel.style.display = ""; // Show input
-            }
-        }
-</script>
 </body>
+
 </html>
+
+
